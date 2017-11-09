9fb5766e8aae65d80cbeb4af09e0592fc674d3ae
Ellis Sutko <esutko@gmail.com>
2017-11-09 10:49:02 -0800

    Update main.dart

--- lib/main/main.dart
+++ lib/main/main.dart
@@ -15,46 +15,12 @@ class GoApp extends StatelessWidget {
   }
 
   static const platform = const MethodChannel('gbg.bluetooth');
-
   PageSelect pageSelect;
-
   final String title;
 
   //Variables for executing the delay before the user can restart the car.
-  Timer timer;
-  Timer bluetoothTimer;
-  FlutterBlue flutterBlue;
   var connection;
   var scanSubscription;
-  bool running = false;
-  int countdown = 0;
-
-  /*Called when the toggle button is pressed. If the car is running, disable it. If the car is not running and the countdown has ended, restart the car. */
-  void toggleStop()
-  {
-    if(running) {
-      running = false;
-      countdown = 5;
-      pageSelect.controlPage.updateState();
-      _startTimer();
-    }
-    else if(countdown == 0) {
-      running = true;
-      pageSelect.controlPage.updateState();
-    }
-  }
-  
-  void _startTimer() {
-    timer = new Timer(const Duration(seconds:1), _timerFinish);
-  }
-
-  void _timerFinish() {
-    countdown--;
-    if(countdown > 0) {
-      _startTimer();
-    }
-    pageSelect.controlPage.updateState();
-  }
 
   @override
   Widget build(BuildContext context) {
