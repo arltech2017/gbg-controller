e4aea51e1d61ddc94323d34297133c2ef422033f
Ellis Sutko <esutko@gmail.com>
2017-11-09 10:29:20 -0800

    Update bluetooth.dart

--- lib/main/utils/bluetooth.dart
+++ lib/main/utils/bluetooth.dart
@@ -1,21 +1,9 @@
 library utils;
 import 'package:flutter/services.dart';
-import 'dart:async';
-	
+
 abstract class Bluetooth {
   scanBt() => local("scan");
   closeBt() => local("close");
   connect() => local("connect");
   toggle() => local("toggle");
-
-  //Connect using flutter_blue, not currently being used
-  void flutterConnect() {
-    DeviceIdentifier id = new DeviceIdentifier("B8:27:EB:9B:89:4E");
-    var device = new BluetoothDevice(id: id);
-
-    print("About to scan");
-    scanSubscription = flutterBlue.scan().listen((scanResult) {
-      print(scanResult.device.id.toString());
-    });
-  }
 }
